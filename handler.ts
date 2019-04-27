ca52955e6eeeb1e536d852c5f912b05f40f97cf1
Lukáš Valenta <lukas.valenta@applifting.cz>
2019-04-27 12:35:58 +0200

    Hello World Slack message

--- handler.ts
+++ handler.ts
@@ -1,12 +1,30 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import Axios from 'axios';
 import 'source-map-support/register';
+import { inspect } from 'util';
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
+export const hello: APIGatewayProxyHandler = async (_event, _context) => {
+  try {
+    const data = {
+      'channel': 'CJ89EFT1N',
+      'text': 'Hello, World!',
+    };
+    const response = await Axios.post('https://slack.com/api/chat.postMessage', data, { headers: { 'Authorization': 'Bearer xoxb-610636479856-608835635555-eKGmoL9HY6q3eih1SNMuOqBt' }});
+      
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+        input: inspect(response, false, 2),
+      }, null, 2),
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        e
+      }, null, 2),
+    }
   };
 }
